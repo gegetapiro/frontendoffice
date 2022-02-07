b73dfb93265a78e580559af8834add90ed590865
gegetapiro <gerardo.trimarco@yahoo.it>
2022-02-07 14:03:52 +0100

    terzo commit

--- searchrecords.php
+++ searchrecords.php
@@ -3,18 +3,19 @@
 include "connection.php";
 
 if ($link) {
-  
+
     $soggetto = $_POST["whosearch"];
+    // $soggetto = "";
     $thequery = "SELECT * FROM `moduloElencoVisitatori` WHERE nome_visitatore LIKE '%" . $soggetto . "%'";
 
     if ($result = mysqli_query($link, $thequery)) {
         $rows = array();
-        if ($result->num_rows > 0) {
-            while ($row = $result->fetch_array(MYSQLI_NUM)) {
-                array_push($rows, $row);
-             }
-         echo json_encode($rows);
+
+        while ($row = $result->fetch_array(MYSQLI_NUM)) {
+            array_push($rows, $row);
         }
+        
+        echo json_encode($rows);
     }
 } else {
     echo "error connection " . error_log($link);
